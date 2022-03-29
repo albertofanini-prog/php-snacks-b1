0898a9afcb4fcf2fcc70f72fb3f677c0b0e0d8fe
albertofanini-prog <albertofanini@outlook.com>
2022-03-29 15:42:47 +0200

    Passare parametri GET

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,3 +1,23 @@
 <?php
-    var_dump('Snack 2');
+    //Passare come parametri GET
+        //Name
+        //Mail
+        //Age
+    //Verificare che
+        //Name: sia più lungo di 3 caratteri
+        //Mail: contenga un punto e una virgola
+        //Age: sia un numero
+    //Se è ok stampare 'Accesso Riuscito' 
+    //Se non è ok stampare 'Accesso Negato' 
+    //var_dump('Snack 2');
+
+    $name = $_GET['name'];
+
+    $mail = $_GET['mail'];
+
+    $age = $_GET['age'];
+
+    var_dump($name);
+    var_dump($mail);
+    var_dump($age);
 ?>
