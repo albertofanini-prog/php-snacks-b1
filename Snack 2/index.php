4167016fe7170a23649afabe52da29cc777f4520
albertofanini-prog <albertofanini@outlook.com>
2022-03-29 15:49:54 +0200

    Check e-mail

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -14,10 +14,42 @@
     $name = $_GET['name'];
 
     $mail = $_GET['mail'];
+    $has_dot = strpos($mail,'.');
+    $has_at = strpos($mail,'@');
 
     $age = $_GET['age'];
 
     var_dump($name);
     var_dump($mail);
+    //var_dump($has_dot);
+    //var_dump($has_at);
     var_dump($age);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+</head>
+<body>
+    <div>
+        <?php
+            if($has_dot && $has_at){
+                ?>
+                    <div>
+                        E-mail valida
+                    </div>
+                <?php
+            } else {
+                ?>
+                    <div>
+                        E-mail non valida
+                    </div>
+                <?php
+            }
+        ?>
+    </div>
+</body>
+</html>
