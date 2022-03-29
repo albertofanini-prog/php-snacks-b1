8b0c953c36bb52237d7041adb7f89f0a950c19fa
albertofanini-prog <albertofanini@outlook.com>
2022-03-29 15:03:48 +0200

    Spiegazione e creazione array

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,3 +1,62 @@
 <?php
-    var_dump('Snack 1');
+    //Creare un array contente delle partite di basket
+        //Ogni array avrà:
+            //Una squadra casa
+            //Una squadra ospite
+            //Punti fatti da squadra casa
+            //Punti fatti da squadra ospite
+    //Stampare a schermo le partite con questo schema:
+        //Olimpia Milano - Cantu | 55 - 60
+    //var_dump('Snack 1');
+
+    $partite = [
+        [
+            'casa' => 'Olimpia Milano',
+            'ospite' => 'Openjobmetis Varese',
+            'punti_casa' => '95',
+            'punti_ospite' => '77',
+        ],
+        [
+            'casa' => 'Virtus Segafredo Bologna',
+            'ospite' => 'UNAhotels Reggio Emilia',
+            'punti_casa' => '78',
+            'punti_ospite' => '70',
+        ],
+        [
+            'casa' => 'GeVi Napoli',
+            'ospite' => 'New Basket Brindisi',
+            'punti_casa' => '79',
+            'punti_ospite' => '69',
+        ],
+        [
+            'casa' => 'Reyer Venezia',
+            'ospite' => 'Bertram Tortona',
+            'punti_casa' => '69',
+            'punti_ospite' => '61',
+        ],
+        [
+            'casa' => 'Dinamo Sassari',
+            'ospite' => 'Fortitudo Bologna',
+            'punti_casa' => '85',
+            'punti_ospite' => '79',
+        ],
+        [
+            'casa' => 'Vanoli Cremona',
+            'ospite' => 'Universo Treviso Basket',
+            'punti_casa' => '74',
+            'punti_ospite' => '67',
+        ],
+        [
+            'casa' => 'VL Pallacanestro Pesaro',
+            'ospite' => 'Germani Basket Brescia',
+            'punti_casa' => '83',
+            'punti_ospite' => '88',
+        ],
+        [
+            'casa' => 'Aquila Basket Trento',
+            'ospite' => 'Allianz Pallacanestro Trieste',
+            'punti_casa' => '75',
+            'punti_ospite' => '74',
+        ],
+    ]
 ?>
